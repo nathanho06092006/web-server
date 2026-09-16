e7f0bab9278dd3ff194eac48ffb2abbb1d1fcd81
Nathan Ho <nathanho@umass.edu>
2026-09-16 23:59:14 +0000

    Initial Express server with two routes

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, web!");
+});
+
+app.get("/hello/:name", (req, res) => {
+  const name = req.params.name;
+  res.send(`Hello, ${name}!`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
